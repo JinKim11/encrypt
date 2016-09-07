327df8deae39219fb86cb3c73c1b5422eda8cc7b
JinKim11 <jennkim1186@gmail.com>
2016-09-06 21:28:04 -0700

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,16 +1,20 @@
 import string
 
+
 alphabet = string.ascii_letters
 punctuation = string.punctuation
 encrypt = {" ":53}
+decrypt = {53: " "}
 
 i = 1
 for letter in alphabet:
-	encrypt[letter] = i 
+	encrypt[letter] = i
+	decrypt[i] = letter
 	i = i + 1 
 z = 54
 for punc in punctuation:
 	encrypt[punc] = z
+	decrypt[z] = punc
 	z = z + 1
 
 def encrypt_message():
@@ -19,11 +23,15 @@ def encrypt_message():
 		print encrypt[message[i]]
 
 def decrypt_message():
-	message = "1 12 9 1 53"  #int(raw_input("Decrypt a message."))
-	for i in range(len(message)):
-		if encrypt[key] == message[i]:
-			print key
+	message = "46 1 12 9 1 53 18 21 12 5 19"  #int(raw_input("Decrypt a message."))\
+
+	for i in message.split(" "):
+		print decrypt[int(i)]
+
+	# for i in range(len(message)):
+	# 	if encrypt[key] == message[i]:
+	# 		print key
 
 
-
+encrypt_message()
 decrypt_message()
